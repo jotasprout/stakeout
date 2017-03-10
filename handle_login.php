36dc37ab936c064c0764fc5044a46c9dd9a3b6cf
jotasprout <jotasprout@gmail.com>
2017-03-09 19:19:56 -0500

    fixed db ref

--- handle_login.php
+++ handle_login.php
@@ -15,16 +15,16 @@ if (isset($_POST['submit'])) {
 		$username=$_POST['username'];
 		$password=$_POST['password'];
 		// Establishing Connection with Server by passing server_name, user_id and password as a parameter
-		$connection = mysql_connect("localhost", "root", "");
+		$connekt = mysql_connect("localhost", "root", "");
 		// To protect MySQL injection for Security purpose
 		$username = stripslashes($username);
 		$password = stripslashes($password);
 		$username = mysql_real_escape_string($username);
 		$password = mysql_real_escape_string($password);
 		// Selecting Database
-		$db = mysql_select_db("jscript_stakeout", $connection);
+		$db = mysql_select_db("jscript_stakeout", $connekt);
 		// SQL query to fetch information of registerd users and finds user match.
-		$query = mysql_query("select * from user_creds where gatoreagle='$password' AND gatorname='$username'", $connection);
+		$query = mysql_query("select * from user_creds where gatoreagle='$password' AND gatorname='$username'", $connekt);
 		$rows = mysql_num_rows($query);
 		if ($rows == 1) {
 			$_SESSION['login_user']=$username; // Initializing Session
@@ -32,7 +32,7 @@ if (isset($_POST['submit'])) {
 			} else {
 				$error = "Username or Password is invalid";
 			}
-		mysql_close($connection); // Closing Connection
+		mysql_close($connekt); // Closing Connection
 	}
 }
 
