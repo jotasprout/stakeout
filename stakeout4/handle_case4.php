f39fdb34f2a32989fd2d5188164c5b84ab0c0848
jotasprout <jotasprout@gmail.com>
2017-03-26 14:20:11 -0400

    further fixes

--- stakeout4/handle_case4.php
+++ stakeout4/handle_case4.php
@@ -27,16 +27,16 @@
 
 // Uses the above instructions for inserting
 //  mysqli_query($pushObserve);
+  $retval = $connekt->query($pushCase);
   
 // Feedback of whether INSERT worked or not
-  $retval = mysqli_query($pushCase, $connekt);
   
   if(!$retval){
 	  die('Crap. Could not open your case: ' . mysql_error());
   }
 	
 	echo "<h2>Case # INSERT NUMBER is now open.</h2>";
-	echo "<p>Assign an investigator or <a href=\"http://www.roxorsoxor.com/stakeout4/insert_observe4.php\">enter an observation</a>.</p>";
+	echo "<p>Either <a href=\"http://www.roxorsoxor.com/stakeout4/cases4.php\">review cases</a> or <a href=\"http://www.roxorsoxor.com/stakeout4/insert_case4.php\">open a new case</a>.</p>";
 	
 // When attempt is complete, connection closes
   mysqli_close($connekt);
--- stakeout4/handle_observe4.php
+++ stakeout4/handle_observe4.php
@@ -1,22 +1,17 @@
 <?php
-// PHP code in a more secure location
-  require_once '../../../php/landfill.php'; // connects to db
+	// PHP code in a more secure location
+  require_once '../../../php/landfill.php';
 
-// Connection test and feedback
-  if (!$connekt)
-  {
-    die('Rats! Could not connect: ' . mysqli_error());
-  }
+  $connekt = new mysqli($db_hostname, $db_username, $db_password, $db_database);
 
-//Uses PHP code to connect to database
-  mysqli_select_db("jscript_stakeout", $connekt);
+  if ($connekt->connect_error) die($connekt->connect_error);
 
-// Assigns form field content to columns in database
+	// Assigns form field content to columns in database
 	$caseNum = mysqli_real_escape_string($connekt, htmlspecialchars($_POST['caseNum']));
 	$action = mysqli_real_escape_string($connekt, htmlspecialchars($_POST['action']));
 	$observation = mysqli_real_escape_string($connekt, htmlspecialchars($_POST['observation']));
 
-// Instructions for inserting form content into database
+	// Instructions for inserting form content into database
   $pushObserve = "
   INSERT INTO observations (
   	caseNum,
@@ -29,20 +24,20 @@
 	'$observation'
 	);";
 
-// Uses the above instructions for inserting
-// mysqli_query($pushObserve);
-  
-// Feedback of whether INSERT worked or not
-  $retval = mysqli_query($pushObserve, $connekt);
-  
+	// Uses the above instructions for inserting
+	// mysqli_query($pushObserve);
+    $retval = $connekt->query($pushObserve);
+	
+	// Feedback of whether INSERT worked or not
+
   if(!$retval){
 	  die('Crap. Could not record your observation: ' . mysqli_error());
   }
 	
 	echo "<h2>Your observation has been recorded.</h2>";
-	echo "<p><a href=\"http://www.jotascript.com/rxrsxr/insertObserveBS.htm\">Record another observation</p>";
+	echo "<p>Either <a href=\"http://www.roxorsoxor.com/stakeout4/observations4.php\">review observations</a> or <a href=\"http://www.roxorsoxor.com/stakeout4/insert_observe4.php\">Record another observation</p>";
 	
-// When attempt is complete, connection closes
+	// When attempt is complete, connection closes
   mysqli_close($connekt);
 
 ?>
