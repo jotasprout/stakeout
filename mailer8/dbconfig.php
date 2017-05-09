c8c5ab08a4c1e8b732427f26289ffb3b7f63bf59
jotasprout <jotasprout@gmail.com>
2017-05-09 06:52:30 -0400

    register and reset plus mail all working

---
+++ mailer8/dbconfig.php
@@ -0,0 +1,28 @@
+<?php
+class Database
+{
+     
+    private $host = "localhost";
+    private $db_name = "jscript_stakeout";
+    private $username = "jscript_jota";
+    private $password = "We2CanFly!";
+    public $conn;
+     
+    public function dbConnection()
+	{
+     
+	    $this->conn = null;    
+        try
+		{
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
+        }
+		catch(PDOException $exception)
+		{
+            echo "Connection error: " . $exception->getMessage();
+        }
+         
+        return $this->conn;
+    }
+}
+?>
