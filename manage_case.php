ed05cc09fafd09918333321b0626016c7536b485
Jota Sprout <jotasprout@gmail.com>
2017-07-14 18:47:06 -0400

    Classing Up Cases & Upgrading Observations

--- manage_case.php
+++ manage_case.php
@@ -3,6 +3,7 @@ session_start();
 require_once 'class.gator.php';
 require_once 'stylesAndSuch.php';
 require_once 'navbar.php';
+
 $user = new USER();
 
 if(!$user->areTheyLoggedIn()) {
@@ -21,32 +22,28 @@ else {
 	if ($connekt->connect_error) die($connekt->connect_error);
 
 	// check if the form has been submitted. If it has, process the form and save it to the database
-	if (isset($_POST['submit']))
-	{
-	// confirm that the 'id' value is a valid integer before getting the form data
-	if (is_numeric($_POST['caseID']))
-		{
-		// get form data, making sure it is valid
-		$caseID = $_POST['caseID'];
-		$caseNum = mysqli_real_escape_string($connekt, htmlspecialchars($_POST['caseNum']));
-		$caseName = mysqli_real_escape_string($connekt, htmlspecialchars($_POST['caseName']));
-		$startDate = mysqli_real_escape_string($connekt, htmlspecialchars($_POST['startDate']));
-		$endDate = mysqli_real_escape_string($connekt, htmlspecialchars($_POST['endDate']));
-		$deliveryDate = mysqli_real_escape_string($connekt, htmlspecialchars($_POST['deliveryDate']));
-		$status = 1;
-
-		if ($endDate !== ''){
-			$status = 0;
-		}
-
-		// check that caseNum and startDate fields are both filled in
-		if ($caseNum == '' || $startDate == '')
-			{
-			// generate error message
-			$error = 'ERROR: Boy, you sure are stupid! Fill in all required fields like you were told!';
+	if (isset($_POST['submit'])) {
+		// confirm that the 'id' value is a valid integer before getting the form data
+		if (is_numeric($_POST['caseID'])) {
+			// get form data, making sure it is valid
+			$caseID = $_POST['caseID'];
+			$caseNum = mysqli_real_escape_string($connekt, htmlspecialchars($_POST['caseNum']));
+			$caseName = mysqli_real_escape_string($connekt, htmlspecialchars($_POST['caseName']));
+			$startDate = mysqli_real_escape_string($connekt, htmlspecialchars($_POST['startDate']));
+			$endDate = mysqli_real_escape_string($connekt, htmlspecialchars($_POST['endDate']));
+			$deliveryDate = mysqli_real_escape_string($connekt, htmlspecialchars($_POST['deliveryDate']));
+			$status = 1;
+	
+			if ($endDate !== ''){
+				$status = 0;
 			}
-		else
-			{
+	
+			// check that caseNum and startDate fields are both filled in
+			if ($caseNum == '' || $startDate == '') {
+				// generate error message
+				$error = 'ERROR: Boy, you sure are stupid! Fill in all required fields like you were told!';
+			}
+			else {
 				// save data to database
 				$updateCase = "UPDATE cases4 SET caseNum='$caseNum', caseName='$caseName', startDate='$startDate', endDate='$endDate', deliveryDate='$deliveryDate', status='$status' WHERE caseID='$caseID'";
 				$retval = $connekt->query($updateCase);
@@ -54,46 +51,44 @@ else {
 				if(!$retval){
 					die('Crap. Could not update your case: ' . mysqli_error());
 				}
-				else
-				{
-				// after save, go to view page
-				header("Location: cases.php");
+				else {
+					// after save, go to view page
+					header("Location: cases.php");
 				}
 			}
 		}
-		else // if the 'id' isn't valid, display an error
-		{
-		echo $error;
+		else {
+			// if the 'id' isn't valid, display an error 
+			echo $error;
 		}
 	}
-	else // if the form hasn't been submitted, get the data from the db and display the form
-	{
+	else {
+		// if the form hasn't been submitted, get the data from the db and display the form 
 		// get 'id' value from URL (if it exists), confirming it is valid and is numeric/larger than 0)
-		if (isset($_GET['id']) && is_numeric($_GET['id']) && $_GET['id'] > 0)
-		{
+		if (isset($_GET['id']) && is_numeric($_GET['id']) && $_GET['id'] > 0) {
 			// query db
 			$caseID = $_GET['id'];
 			$result = mysqli_query($connekt, "SELECT * FROM cases4 WHERE caseID=$caseID")
 			or die(mysqli_error($result));
 			$row = mysqli_fetch_array($result);
 			// check that the 'id' matches up with a row in the databse
-			if($row)
-				{
+			if($row) {
 				// get data from db
 				$caseID = $row['caseID'];
 				$caseNum = $row['caseNum'];
 				$caseName = $row['caseName'];
-				$startDate = $row['startDate'];
+				$startDate = new DateTime($row["startDate"] ." UTC");
+				$startDate ->setTimezone(new DateTimeZone('America/New_York')); 
 				$endDate = $row['endDate'];
 				$deliveryDate = $row['deliveryDate'];
-				}
-			else // if no match, display result
-				{
-					echo "No results!";
-				}
+			}
+			else {
+				// if no match, display result
+				echo "No results!";
+			}
 		}
-		else // if the 'id' in the URL isn't valid, or if there is no 'id' value, display an error
-		{
+		else {
+			// if the 'id' in the URL isn't valid, or if there is no 'id' value, display an error 
 			echo $error;
 		}
 	}
@@ -134,60 +129,14 @@ function truncomatic ($textytext, $endomatic, $linkylink) {
 </nav> <!-- /navbar -->	
 
 <div class="panel panel-primary">
-	<div class="panel-heading"><h3 class="panel-title">Manage Case</h3></div>
+	<div class="panel-heading"><h3 class="panel-title">Case: <?php echo $caseName; ?></h3></div>
 		<div class="panel-body">
 		<!-- Panel Content -->
-		<p>*Required</p>
-        <form class="form-horizontal" action="" method="post">
-        	<input type="hidden" name="caseID" value="<?php echo $caseID; ?>"/>
-            <fieldset>
-                <div class="form-group"> <!-- Row 1 -->
-                    <!-- Column 1 -->
-                    <label class="col-lg-2 control-label" for="caseNum">Case Number*</label>
-                    <!-- Column 2 -->
-                    <div class="col-lg-4">
-                        <input class="form-control" type="text" name="caseNum" value="<?php echo $caseNum; ?>"/>
-                    </div>
-                </div><!-- /Row 1 -->
-                <div class="form-group"> <!-- Row 2 -->
-                    <!-- Column 1 -->
-                    <label class="col-lg-2 control-label" for="caseName">Case Name*</label>
-                    <!-- Column 2 -->
-                    <div class="col-lg-4">
-                        <input class="form-control" type="text" name="caseName" value="<?php echo $caseName; ?>" />
-                    </div>
-                </div><!-- /Row 2 -->
-                <div class="form-group"> <!-- Row 3 -->
-                    <!-- Column 1 -->
-                    <label class="col-lg-2 control-label" for="startDate">Start Date*</label>
-                    <!-- Column 2 -->
-                    <div class="col-lg-4">
-                        <input class="form-control" type="date" name="startDate" value="<?php echo $startDate; ?>" readonly/>
-                    </div>
-                </div><!-- /Row 3 -->
-                <div class="form-group"> <!-- Row 4 -->
-                    <!-- Column 1 -->
-                    <label class="col-lg-2 control-label" for="endDate">End Date</label>
-                    <!-- Column 2 -->
-                    <div class="col-lg-4">
-                        <input class="form-control" type="date" id="datepicker" name="endDate" placeholder="YYYY-MM-DD" value="<?php echo $endDate; ?>" />
-                    </div>
-                </div><!-- /Row 4 -->
-                <div class="form-group"> <!-- Row 5 -->
-                    <!-- Column 1 -->
-                    <label class="col-lg-2 control-label" for="deliveryDate">Delivery Date</label>
-                    <!-- Column 2 -->
-                    <div class="col-lg-4">
-                        <input class="form-control" type="date" id="datepicker" name="deliveryDate"  placeholder="YYYY-MM-DD" value="<?php echo $deliveryDate; ?>" />
-                    </div>
-                </div><!-- /Row 5 -->
-                <div class="form-group"> <!-- Last Row -->
-                    <div class="col-lg-4 col-lg-offset-2">
-                        <button class="btn btn-primary" type="submit" name="submit">Update Case</button>
-                    </div>
-                </div><!-- /Last Row -->
-            </fieldset>
-        </form>
+		<?php 
+			if ($jefe == 1) {
+				include 'case_mgmt.php';
+			}
+		?>
 		
 <div class="well">
 		
@@ -244,16 +193,15 @@ function truncomatic ($textytext, $endomatic, $linkylink) {
 					ON a.username = b.username
 				INNER JOIN cases4 c
 					ON a.caseID = c.caseID
-						WHERE a.caseID = '$caseID'"; 				
+						WHERE a.caseID = '$caseID' AND a.username = '$username'"; 				
     // Use variable with MySQL command to grab info from database
 	$result = $connekt->query($query);
     // Start creating an HTML table and create header row
     echo "<table class='table table-striped table-hover'>";
-    echo "<thead><tr><th>Investigator</th><th>Observation</th><th>Date</th></tr></thead><tbody>";
+    echo "<thead><tr><th>Observation</th><th>Date</th></tr></thead><tbody>";
     // Create a row in HTML table for each row from database
     while ($row = mysqli_fetch_array($result)) {
         echo "<tr>";
-		echo "<td>" . $row['surname'] . "</td>";
 
 		$thisObserve = $row["observation"];
 		$observeLength = strlen($thisObserve);
