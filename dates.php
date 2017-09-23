8f32bbefc3d72308137735106b8f644b2496aea7
Jota Sprout <jotasprout@gmail.com>
2017-09-23 17:35:20 -0400

    Case Dates in-progress

---
+++ dates.php
@@ -0,0 +1,6 @@
+<?php
+
+    // the following is just notes, I don't expect it to work    
+    $startDate = db_quote(date('Y-m-d',strtotime($_POST['startDate'])));
+
+?>
--- js/dates.js
+++ js/dates.js
@@ -3,3 +3,5 @@
         $('#startDate').datepicker({dateFormat: 'yy-mm-dd'});
     });
 </script>
+
+// https://stackoverflow.com/questions/12120433/php-mysql-insert-date-format
