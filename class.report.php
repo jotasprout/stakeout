0af6fc1e54f2dbf290735c04b63bf82646fa0b42
Jota Sprout <jotasprout@gmail.com>
2017-09-26 21:01:14 -0400

    Building Cover Report

--- class.report.php
+++ class.report.php
@@ -24,6 +24,35 @@ class REPORT {
 		return $stmt;
 	}
 
+	public function reportCase() {
+		if (isset($_GET['id']) && is_numeric($_GET['id']) && $_GET['id'] > 0) {
+			// query db
+			$caseID = $_GET['id'];
+			$result = mysqli_query($connekt, "SELECT * FROM cases WHERE caseID=$caseID")
+			or die(mysqli_error($result));
+			$row = mysqli_fetch_array($result);
+			// check that the 'id' matches up with a row in the databse
+			if($row) {
+				// get data from db
+				$caseID = $row['caseID'];
+				$caseNum = $row['caseNum'];
+				$caseName = $row['caseName'];
+				$startDate = new DateTime($row["startDate"] ." UTC");
+				$startDate ->setTimezone(new DateTimeZone('America/New_York')); 
+				$endDate = $row['endDate'];
+				$deliveryDate = $row['deliveryDate'];
+			}
+			else {
+				// if no match, display result
+				echo "No results!";
+			}
+		}
+		else {
+			// if the 'id' in the URL isn't valid, or if there is no 'id' value, display an error 
+			echo $error;
+		}
+	}
+
 	// make this Cover
 	public function register($uname,$forename,$surname,$email,$code) {
 		try {
