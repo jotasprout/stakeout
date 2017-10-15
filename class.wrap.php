9bde7447607c02b1660a41c60c9650bcd332272d
Jota Sprout <jotasprout@gmail.com>
2017-10-15 17:03:55 -0400

    Still Trying ZIP and Download

--- class.wrap.php
+++ class.wrap.php
@@ -5,14 +5,25 @@ class collection {
 
     private $collection;
 
-    public function __construct ($assetName, $putHere) {
+    public $caseIDfolder = 'caseAssets/' . $caseID . '/';
+
+    public $putHere = 'caseAssets/';
+
+    public function __construct ($caseID, $putHere) {
         $this -> collection = new ZipArchive ();
 
     }
 
     public function collectCaseFolder ($caseIDfolder) {
         if (is_dir($caseIDfolder)) {
-            // create the archive
+
+            $targetDir = opendir($caseIDfolder);
+
+            $this -> collection -> addEmptyDir ($caseID);
+
+            while ( ($file = readdir($targetDir) ) !== false) {
+                $this -> add_file ($targetDir . '/' . $file);
+            }
         }
 
         else {
