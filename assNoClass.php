baf93367ffe19c2338461f111df0506e77f6ab8d
Jota Sprout <jotasprout@gmail.com>
2017-10-16 04:26:27 -0400

    assNoClass

---
+++ assNoClass.php
@@ -0,0 +1,40 @@
+<?php
+
+$caseID = '8';
+
+$newAssetsArchive = new ZipArchive();
+
+$newZIPfileName = $caseID . '.zip';
+
+$newAssetsArchive -> open ('caseAssets/' . $newZIPfileName, ZipArchive::CREATE);
+
+// create new folder in zip file named after $caseID
+$newAssetsArchive -> addEmptyDir ($caseID)
+        
+$getThis = 'caseAssets/' . $caseID . '/';
+
+
+// if $getThis exists
+if (is_dir($getThis)) {
+
+    // add empty folder named after $caseID inside the archive
+    $newAssetsArchive -> addEmptyDir ($caseID);
+
+    // start looking at items inside the original assets folder for $caseID
+    $targetDir = opendir($getThis);
+
+    // while there is at least one more file in the original folder
+    while ( ($file = readdir($targetDir) ) !== false) {
+        // add a copy of each item in original folder to the new $caseID folder in the archive
+        $newAssetsArchive -> addFile ($caseID . '/' . $file);
+    }
+}
+
+else {
+    $msg = 'There are no assets for this case.';
+    // show that message
+}
+
+$newAssetsArchive -> close();
+
+?>
